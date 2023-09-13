92879ea585184b879c67fc43f597492149c44115
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-09-13 11:48:30 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by İbrahim Bayram on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
